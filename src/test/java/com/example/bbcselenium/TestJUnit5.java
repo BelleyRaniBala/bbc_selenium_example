406b32ac16111903667a05e5f5c11d95c2247cc8
belleyrani.bala <balabelley@gmail.com>
2018-06-01 19:09:38 +0100

    JUnit5 works

---
+++ src/test/java/com/example/bbcselenium/TestJUnit5.java
@@ -0,0 +1,13 @@
+package com.example.bbcselenium;
+
+import org.junit.jupiter.api.Test;
+
+public class TestJUnit5 {
+
+    @Test
+    public void test() {
+
+        System.out.print("Belley");
+    }
+
+}
